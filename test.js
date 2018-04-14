c29751fe0622f2a8bc91efbc88e5242282504c68
Titus Wormer <tituswormer@gmail.com>
2018-04-14 13:04:15 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
-'use strict';
+'use strict'
 
-var test = require('tape');
-var collapse = require('.');
+var test = require('tape')
+var collapse = require('.')
 
-test('collapseWhiteSpace(value)', function (t) {
-  t.equal(collapse(true), 'true', 'should coerce to string');
+test('collapseWhiteSpace(value)', function(t) {
+  t.equal(collapse(true), 'true', 'should coerce to string')
 
-  t.equal(collapse(' \t\nbar \nbaz\t'), ' bar baz ');
-  t.equal(collapse('   bar\t\t\tbaz\n\n\n'), ' bar baz ');
-  t.equal(collapse(' \n bar\t\n\tbaz\r\n'), ' bar baz ');
+  t.equal(collapse(' \t\nbar \nbaz\t'), ' bar baz ')
+  t.equal(collapse('   bar\t\t\tbaz\n\n\n'), ' bar baz ')
+  t.equal(collapse(' \n bar\t\n\tbaz\r\n'), ' bar baz ')
 
-  t.end();
-});
+  t.end()
+})
